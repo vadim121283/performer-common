21f7b1e117d9d035b99c75d32fcd4b55becb6d65
Vadim Akhmatov <vadim121283@gmail.com>
2022-03-10 23:48:08 +0500

    add_profile_team

---
+++ domain/entity/Profile.ts
@@ -0,0 +1,5 @@
+export interface Profile {
+  name: string;
+  surname: string;
+  teamId: UniqueId;
+}
--- domain/entity/User.ts
+++ domain/entity/User.ts
@@ -1,5 +1,8 @@
+import { Profile } from './Profile';
+
 export interface User {
   guid: UniqueId;
   login: string;
   token?: string;
+  profile?: Profile;
 }
